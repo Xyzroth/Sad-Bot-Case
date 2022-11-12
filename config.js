a9953d2858307134239bd105888b29bd0b1ec265
Xyzroth <116151791+Xyzroth@users.noreply.github.com>
2022-11-12 20:54:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6282287219167']
-global.ownernomer = "6282287219167"
-global.premium = ['6282287219167']
+global.owner = ['6285215511208]
+global.ownernomer = "6285215511208"
+global.premium = ['6285215511208']
 global.packname = 'Sticker By'
-global.author = 'Sad-Bot'
+global.author = 'Natsu-Bot'
 global.sessionName = 'papah'  //jangan diganti bro nanti error
 global.jumlha = '999'
 global.jumhal = '100000000000000'
@@ -65,9 +65,9 @@ global.myytv = ''
 global.mygc = ""
 
 // Engak Ngaruh, Belum Kepasang
-global.botname = 'SAD BOT'
-global.akulaku = 'Bot By Papah'
-global.ytname = 'YT PAPAH' //Kalo Ngk Punya Yt Ngak Usah Di Ganti
+global.botname = 'NATSU BOT'
+global.akulaku = 'Bot By XyzxyyRoth'
+global.ytname = 'YT XYZROTH' //Kalo Ngk Punya Yt Ngak Usah Di Ganti
 
 
 let file = require.resolve(__filename)
